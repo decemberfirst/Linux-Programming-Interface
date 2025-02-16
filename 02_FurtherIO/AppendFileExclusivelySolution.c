d1fcc0c86ba5b633d22ba21ab8c714e9f548e82d
Rohan <ping.techyrohan@gmail.com>
2025-02-16 20:07:07 +0545

    Excl File Append

---
+++ 02_FurtherIO/AppendFileExclusivelySolution.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+void append_data_without_sync(char *messgae)
+{
+    int fd = open("file.txt", O_RDWR | O_APPEND);
+    if (fd == -1)
+    {
+        perror("Open");
+        exit(1);
+    }
+
+    lseek(fd, 0, SEEK_END);
+
+    sleep(5);
+    write(fd, messgae, strlen(messgae));
+    close(fd);
+}
+
+int main()
+{
+    append_data_without_sync("First Process \n");
+    append_data_without_sync("Second Process \n");
+    return 0;
+}
