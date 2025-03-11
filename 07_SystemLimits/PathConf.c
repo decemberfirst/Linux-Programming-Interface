4b27c1301d85899137fc3071167fa772ba5c5cff
Rohan <ping.techyrohan@gmail.com>
2025-03-11 21:14:47 +0545

    Pipe Buffer Size

--- 07_SystemLimits/PathConf.c
+++ 07_SystemLimits/PathConf.c
@@ -5,5 +5,7 @@
 int main() {
     printf("Maximum number of chars in file %ld \n",pathconf("/", _PC_NAME_MAX));
     printf("Maximum number of chars in path %ld \n",pathconf("/", _PC_PATH_MAX));
+    // If you write up to this size, the write will be atomic — meaning the entire write operation will either succeed completely or fail completely (no partial writes).
+    printf("Maximum pipe buffer size %ld \n",pathconf("/", _PC_PIPE_BUF)); //
     return 0;
 }
