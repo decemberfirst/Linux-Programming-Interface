2c64783770b6405ba12bd27527a1154c9e7df1d8
Rohan <ping.techyrohan@gmail.com>
2025-03-15 23:29:54 +0545

    High Resolution Timer

---
+++ 12_TimersAndSleeping/HighResolutionTimer.c
@@ -0,0 +1,24 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include<time.h>
+// CLOCK_REALTIME GIVES EPOCH TIME WITH NANOSECOND PRECISION
+// CLOCK MONOTONIC GIVES TIME SINCE BOOT WITH NANOSECOND PRECISION
+// CLOCK_PROCESS_CPUTIME_ID GIVES TIME SPENT BY PROCESS IN USER AND KERNEL MODE
+// CLOCK_THREAD_CPUTIME_ID GIVES TIME SPENT BY THREAD IN USER AND KERNEL MODE
+int main() {
+    struct timespec startTime;
+    // // clock_gettime(CLOCK_REALTIME, &startTime);
+    // time_t epoch = time(NULL);
+    // printf("Epoch Time: %ld\n", epoch);
+    // printf("Epoch with nanosecond precision %ld.%09ld\n", startTime.tv_sec, startTime.tv_nsec);
+
+    // clock_gettime(CLOCK_MONOTONIC, &startTime);
+    // printf("MONOTONIC Time: %ld.%09ld\n", startTime.tv_sec, startTime.tv_nsec);
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &startTime);
+    printf("PROCESS CPU Time: %ld.%09ld\n", startTime.tv_sec, startTime.tv_nsec);
+
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &startTime);
+    printf("THREAD CPU Time: %ld.%09ld\n", startTime.tv_sec, startTime.tv_nsec);
+    return 0;
+}
